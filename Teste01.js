d0331f05af6303be584f4caac9659dbd8c554eec
Vic-NG <victor.gojlevicius@gmail.com>
2020-03-07 18:03:22 -0400

    primeiro envio

---
+++ Teste01.js
@@ -0,0 +1,13 @@
+function validar(formTeste){
+
+        for(i = 0; i < 3; i++) {
+            if (formTeste.nome.value[i] == " ") {
+                alert("Nome inválido.");
+                formTeste.nome.focus();
+                return false;
+            }
+        }
+        alert("formularioValido");
+        return true;
+    
+}
