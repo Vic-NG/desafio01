0f400a98d65fa9f970c8a911bf8a367c60ed8cfb
Vic-NG <victor.gojlevicius@gmail.com>
2020-03-12 21:56:34 -0400

    Código JS

--- Teste01.js
+++ Teste01.js
@@ -1,13 +1,17 @@
 function validar(formTeste){
 
-        for(i = 0; i < 3; i++) {
+    if(formTeste.sexo.value == ""){
+        alert("Selecione um genero.")
+        return false;
+    }
+    
+    for(i = 0; i < 3; i++) {
             if (formTeste.nome.value[i] == " ") {
                 alert("Nome inválido.");
                 formTeste.nome.focus();
                 return false;
-            }
+               }
         }
         alert("formularioValido");
-        return true;
-    
+        return true; 
 }
